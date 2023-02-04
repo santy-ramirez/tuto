1ff4df90e156c51e3e917c30c100a14e98db30c8
santiago <santysjr956@gmail.com>
2023-02-04 13:08:26 -0300

    add structure  project

---
+++ src/main/java/com/saniago/tutorial/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.saniago.tutorial.repository;
+
+import com.saniago.tutorial.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/com/saniago/tutorial/service/UserService.java
@@ -0,0 +1,7 @@
+package com.saniago.tutorial.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
